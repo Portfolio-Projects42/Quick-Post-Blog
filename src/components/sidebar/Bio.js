1c9b813779a8043e1cdbdc1be1b1fc890b48c03e
Stackbit Code Editor <projects@stackbit.com>
2021-06-11 12:27:13 +0000

    Bio.js: changed

--- src/components/sidebar/Bio.js
+++ src/components/sidebar/Bio.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./sidebar.css"
 
-import willjw3 from "../../images/willjw3.jpg"
+import willjw3 from "../../images/bgoon.png"
 
 const Bio = ({ author, tagline }) => {
 
